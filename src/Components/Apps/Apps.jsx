2578ea1e3be868433d12d70f0d66c80a74589631
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-08 18:19:34 +0600

    showed apps in the apps page dynamically with app count and search box

--- src/Components/Apps/Apps.jsx
+++ src/Components/Apps/Apps.jsx
@@ -1,9 +1,15 @@
+import { Suspense } from 'react';
+import Loader from '../Loading';
+import Applications from './Applications';
 import Appshero from './Appshero';
 
 const Apps = () => {
     return (
         <>
         <Appshero></Appshero>
+        <Suspense fallback={<Loader></Loader>}>
+            <Applications></Applications>
+        </Suspense>
         </>
     );
 };
