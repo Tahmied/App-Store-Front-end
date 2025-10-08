573090507c996f547e942b544ef383fdc82774de
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-10-08 14:33:59 +0600

    - completed hero section for all pages

--- src/Components/Installation/InstallationHero.jsx
+++ src/Components/Installation/InstallationHero.jsx
@@ -4,8 +4,8 @@ const InstallationHero = () => {
         <>
         <section className="app-hero">
             <div className="app-hero-container">
-                <p className="app-hero-title">Our All Applications</p>
-                <p className="app-hero-description">Explore All Apps on the Market developed by us. We code for Millions</p>
+                <p className="app-hero-title">Your Installed Apps</p>
+                <p className="app-hero-description">Explore All Apps on the Market developed by us</p>
             </div>
         </section>
         </>
